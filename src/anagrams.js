c9afaef4616ce21c82b436d56f2b511cc0569ff6
fdavison1 <fred.noreply@gmail.com>
2020-02-19 18:44:15 -0600

    anagrams

--- src/anagrams.js
+++ src/anagrams.js
@@ -1,1 +1,7 @@
-export const checkForAnagram = () => {};
+export const checkForAnagram = (string1, string2) => {
+
+    const string1Letters = string1.toLowerCase().split('');
+    const string2Letters = string2.split('');
+
+    return string1Letters.every(letter => string2Letters.includes(letter));
+};
--- src/anagrams.test.js
+++ src/anagrams.test.js
@@ -1,3 +1,21 @@
 import {checkForAnagram} from './anagrams';
 import {expect} from 'chai';
 
+describe('anagrams function', () => {
+    it('should return false if not an anagram', () => {
+        const actual = checkForAnagram('cat', 'dog');
+        expect(actual).to.be.false;
+    });
+    it('should return true if anagram', () => {
+        const actual = checkForAnagram('listen', 'silent');
+        expect(actual).to.be.true;
+    });
+    it('conversation and voices rant on', () => {
+        const actual = checkForAnagram('conversation', 'voices rant on');
+        expect(actual).to.be.true;
+    });
+    it('STATE and taste are anagrams', () => {
+        const actual = checkForAnagram('STATE', 'taste');
+        expect(actual).to.be.true;
+    });
+});
