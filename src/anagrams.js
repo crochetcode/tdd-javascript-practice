0ce45a1a395e54c539c771d634dab342479a5611
fdavison1 <fred.noreply@gmail.com>
2020-02-19 18:59:25 -0600

    anagrams 2

--- src/anagrams.js
+++ src/anagrams.js
@@ -1,7 +1,17 @@
+import {isEqual} from 'lodash';
+import {getLetterCount} from './letter-count';
+
 export const checkForAnagram = (string1, string2) => {
 
-    const string1Letters = string1.toLowerCase().split('');
-    const string2Letters = string2.split('');
+    string1 = string1.split(' ').join('')
+    string2 = string2.split(' ').join('')
+    
+    string1 = string1.toLowerCase();
+    string2 = string2.toLowerCase();
+    console.log(string2);
 
-    return string1Letters.every(letter => string2Letters.includes(letter));
+    const string1LetterCount = getLetterCount(string1);
+    const string2LetterCount = getLetterCount(string2);
+
+    return isEqual(string1LetterCount, string2LetterCount);
 };
